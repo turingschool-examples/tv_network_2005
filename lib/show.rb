31bb27b5809496fa73d7d00b4d33a74d25936a88
Sienna Kopf <princess.kopf@gmail.com>
2020-05-19 09:30:01 -0600

    Add show functionality for total sal, highest paid actor, and actors array

--- lib/show.rb
+++ lib/show.rb
@@ -1,12 +1,30 @@
 class Show
   attr_reader :name,
               :creator,
-              :characters 
+              :characters
 
   def initialize(name, creator, characters)
     @name = name
     @creator = creator
     @characters = characters
+  end
+
+  def total_salary
+    characters.reduce(0) do |sum, character|
+      sum += character.salary
+    end
+  end
+
+  def highest_paid_actor
+    characters.max_by do |character|
+      character.salary
+    end.actor
+  end
+
+  def actors
+    characters.map do |character|
+      character.actor
+    end 
   end
 
 end
