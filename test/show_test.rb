81a3faad5105f13b6816702a8a81b680a899e53c
Phillip Strom <phillip.strom@gmail.com>
2020-05-19 09:21:58 -0600

    Create instance test

--- test/show_test.rb
+++ test/show_test.rb
@@ -5,8 +5,8 @@ require 'minitest/pride'
 
 class ShowTest < Minitest::Test
   def test_it_exists
-    # kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
-    # michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
 
     assert_instance_of Show, knight_rider
