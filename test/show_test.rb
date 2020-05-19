5d1db134e753db7e5db91ee79841a0a13a386820
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-19 09:47:14 -0600

    test show has actor names

--- test/show_test.rb
+++ test/show_test.rb
@@ -41,32 +41,13 @@ class ShowTest < Minitest::Test
     assert_equal "David Hasselhoff", knight_rider.highest_paid_actor
   end
 
+  def test_show_actor_names
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [kitt, michael_knight])
+
+    assert_equal [ "William Daniels","David Hasselhoff"], knight_rider.actors
+  end
+
 end
-
-
-# pry(main)> kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
-# # => #<Character:0x00007f98a4ba8dc8...>
-#
-# pry(main)> michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
-# # => #<Character:0x00007f8327213de0...>
-#
-# pry(main)> knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
-# # => #<Show:0x00007f83280b3288...>
-#
-# pry(main)> knight_rider.name
-# # => "Knight Rider"
-#
-# pry(main)> knight_rider.creator
-# # => "Glen Larson"
-#
-# pry(main)> knight_rider.characters
-# # => [#<Character:0x00007f8327213de0...>, #<Character:0x00007f8326ab57d8...>]
-#
-# pry(main)> knight_rider.total_salary
-# # => 2600000
-#
-# pry(main)> knight_rider.highest_paid_actor
-# # => "David Hasselhoff"
-#
-# pry(main)> knight_rider.actors
-# # => ["David Hasselhoff", "William Daniels"]
