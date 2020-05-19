d3a6855d844ab6a666f4c00c9ee1aad5c80b7cb1
Phillip Strom <phillip.strom@gmail.com>
2020-05-19 09:22:18 -0600

    Create initialize method

--- lib/show.rb
+++ lib/show.rb
@@ -1,4 +1,11 @@
 require './lib/character'
 
 class Show
+attr_reader :name, :creator, :characters
+
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
 end
