08524ff5db116492b989ce4fd624042a6c55fa09
Amy Moller <amy.moller@yahoo.com>
2020-05-19 10:13:39 -0600

    Add more methods

--- lib/show.rb
+++ lib/show.rb
@@ -1,9 +1,17 @@
 class Show
 
-  attr_reader :
+  attr_reader :characters,
+              :creator,
+              :total_salary,
+              :highest_paid_actor,
+              :actors
 
-  def initialize()
-
+  def initialize(show)
+    @characters = characters
+    @creator = creator
+    @total_salary = total_salary
+    @highest_paid_actor = highest_paid_actor
+    @actors = actors
   end
 
 
