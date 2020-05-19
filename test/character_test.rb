333bb9316f34fe520d4b0ee252e7110b373b3523
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-19 11:07:17 -0400

    Add test it has a name

--- test/character_test.rb
+++ test/character_test.rb
@@ -4,8 +4,14 @@ require './lib/character'
 
 class CharacterTest < MiniTest::Test
   def test_it_exists
-    character = Character.new
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
 
-    assert_instance_of Character, character
+    assert_instance_of Character, kitt
+  end
+
+  def test_it_has_a_name
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal "KITT", kitt.name
   end
 end
