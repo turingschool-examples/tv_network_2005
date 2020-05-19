3e11cd8dca6a9665c11946d351d125f7fd87e3d4
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-19 09:17:11 -0600

    Create initialized method tests

--- test/show_test.rb
+++ test/show_test.rb
@@ -14,4 +14,16 @@ class ShowTest < Minitest::Test
   def test_it_exists
     assert_instance_of Show, @knight_rider
   end
+
+  def test_it_has_a_name
+    assert_equal "Knight Rider", @knight_rider.name
+  end
+
+  def test_it_has_a_creator
+    assert_equal "Glen Larson", @knight_rider.creator
+  end
+
+  def test_it_hace_characters_array
+    assert_equal [@michael_knight, @kitt]
+  end
 end
