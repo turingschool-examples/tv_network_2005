bf964652e260d62aeeead32d746355e23986ef43
Sienna Kopf <princess.kopf@gmail.com>
2020-05-19 09:13:45 -0600

    Add functionality for name actor and salary readers

--- lib/character.rb
+++ lib/character.rb
@@ -1,6 +1,12 @@
 class Character
+  attr_reader :name,
+              :actor,
+              :salary
 
   def initialize(attributes)
+    @name = attributes[:name]
+    @actor = attributes[:actor]
+    @salary = attributes[:salary]
   end
 
 end
