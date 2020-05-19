2a689b72418d62a493355cd6055be220e22bba1a
Ryan Laleh <laleh21@yahoo.com>
2020-05-19 10:55:35 -0600

    Final commit

---
+++ lib/network.rb
@@ -0,0 +1,15 @@
+class Network
+  attr_reader :name, :shows
+
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def main_characters
+  end
+end
---
+++ test/network_test.rb
@@ -0,0 +1,41 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+require './lib/network'
+
+class NetworkTest < Minitest::Test
+
+  def setup
+    @nbc = Network.new("NBC")
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
+  end
+
+  def test_it_exists
+    assert_instance_of Network, @nbc
+  end
+
+  def test_it_has_name
+    assert_equal "NBC", @nbc.name
+  end
+
+  def test_it_has_shows
+    assert_equal [], @nbc.shows
+  end
+
+  def test_it_adds_show
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+    assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
+  end
+
+  def test_it_has_main_characters
+    @nbc.add_show(@knight_rider)
+    assert_equal [@kitt], @nbc.main_characters
+  end
+end
