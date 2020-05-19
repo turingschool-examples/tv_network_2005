24754258dbc1d7fc19a791512c562538ed8500fb
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-19 09:23:18 -0600

    Create show class and test for show attributes; name, creator, characters.

---
+++ test/show_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+
+
+class ShowTest < Minitest::Test
+
+  def test_show_has_name_and_creator
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    
+
+  end
+
+
+
+
+end
