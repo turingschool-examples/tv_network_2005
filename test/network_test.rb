a4521e2e88683ce59c2b0f7115b7356e1d46f32a
Dan Sehl <dan.sehl@gmail.com>
2020-05-19 09:34:06 -0600

    Write passing add multiple shows test

--- test/network_test.rb
+++ test/network_test.rb
@@ -33,4 +33,19 @@ class NetworkTest < Minitest::Test
     assert_equal [knight_rider], nbc.shows
   end
 
+  def test_it_can_add_multiple_shows
+    nbc = Network.new("NBC")
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+
+    assert_equal [knight_rider, parks_and_rec], nbc.shows
+  end
+
 end
