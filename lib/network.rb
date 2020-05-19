2ce13566819d5e7beb08c0aae732270e7b9b75cb
Michael Chickillo <truthvendor@gmail.com>
2020-05-19 10:58:07 -0600

    Initial commit

---
+++ lib/network.rb
@@ -0,0 +1,20 @@
+class Network
+  attr_reader :name,
+              :shows
+
+  def initialize(name)
+    @name = name
+    @shows = []
+  end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def main_characters
+    @shows.map do |show|
+      show.characters
+    end.flatten
+  end
+
+end
