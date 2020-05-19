246967196e07f214c6d3b0558715d7a124687253
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-19 11:14:10 -0400

    Add show attributes and test show exists

--- test/show_test.rb
+++ test/show_test.rb
@@ -4,5 +4,13 @@ require './lib/character'
 require './lib/show'
 
 class ShowTest < MiniTest::Test
+  def test_it_exists
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_instance_of Show, knight_rider
+  end
+
   
 end
