b69252f0537d74dc8556ad3e6d90872fe8fe3e10
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-19 10:22:20 -0600

    Test network can add shows

--- test/network_test.rb
+++ test/network_test.rb
@@ -17,5 +17,32 @@ class NetworkTest < Minitest::Test
     assert_equal [], nbc.shows
   end
 
+  def test_network_can_add_shows
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    nbc = Network.new("NBC")
+
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+
+    assert_equal [knight_rider, parks_and_rec], nbc.shows
+  end
+
+  def test_network_main_characters
+    skip
+  end
+
+  def test_actors_by_show
+    skip
+
+    assert_equal {knight_rider: ["David Hasselhoff", "William Daniels"], parks_and_rec: ["Amy Poehler", "Nick Offerman"]},nbc.actors_by_show
+  end
+
 
 end
