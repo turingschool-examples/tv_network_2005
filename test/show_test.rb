66354c40d53c7c47bd73a6f4174b61e8bb300f2b
Dani Coleman <danicoleman00@gmail.com>
2020-05-19 10:55:01 -0600

    Remove pry

--- test/show_test.rb
+++ test/show_test.rb
@@ -22,8 +22,8 @@ class ShowTest < Minitest::Test
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
     knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
-
-      assert_equal 2_600_000, knight_rider.total_salary
+  
+    assert_equal 2_600_000, knight_rider.total_salary
   end
 
 end
