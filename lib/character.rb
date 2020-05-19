200349bdc27cb25d38191f2b314f4cc9dc55a454
Judith Pillado <judith.pillado@gmail.com>
2020-05-19 10:24:29 -0500

    Iteration 1

---
+++ lib/character.rb
@@ -0,0 +1,10 @@
+class Character
+  attr_reader :name, :actor, :salary 
+
+  def initialize(info)
+    @name   = info[:name]
+    @actor  = info[:actor]
+    @salary = info[:salary]
+  end
+
+end
---
+++ test/character_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+
+class CharacterTest < Minitest::Test
+
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+  end
+
+  def test_it_exists
+    assert_instance_of Character, @kitt
+  end
+
+  def test_character_information
+    assert_equal "KITT", @kitt.name
+    assert_equal "William Daniels", @kitt.actor
+    assert_equal 1000000, @kitt.salary
+  end 
+
+end
