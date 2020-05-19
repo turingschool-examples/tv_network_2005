9817958b26c931a6959c36edce4c226dcedded54
Kathy Bui <kathy.bui1992@gmail.com>
2020-05-19 09:10:33 -0600

    Create initialize method and pass all tests

--- lib/character.rb
+++ lib/character.rb
@@ -1,6 +1,11 @@
 class Character
-  attr_reader
+  attr_reader :name,
+              :actor,
+              :salary
 
   def initialize(character_params)
+    @name = character_params[:name]
+    @actor = character_params[:actor]
+    @salary = character_params[:salary]
   end
 end
