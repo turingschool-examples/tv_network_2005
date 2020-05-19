b2b320f09eb43d403229bb2070b7ba3a19886c96
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-19 11:08:56 -0400

    Add tests for actor and salary

--- test/character_test.rb
+++ test/character_test.rb
@@ -14,4 +14,16 @@ class CharacterTest < MiniTest::Test
 
     assert_equal "KITT", kitt.name
   end
+
+  def test_it_has_an_actor
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal "William Daniels", kitt.actor
+  end
+
+  def test_it_has_a_salary
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+
+    assert_equal 1000000, kitt.salary
+  end
 end
