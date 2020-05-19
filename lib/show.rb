6171a61c852470fbc578336616725aa87a1a7093
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-19 10:03:07 -0600

    Iteration 2 complete

---
+++ lib/show.rb
@@ -0,0 +1,31 @@
+class Show
+
+  attr_reader :name,
+              :creator,
+              :characters,
+              :actors
+
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+
+  end
+
+  def total_salary
+    @total_salary = @characters[0].salary + @characters[1].salary
+  end
+
+  def highest_paid_actor
+    @characters.max_by do |character|
+      character.salary
+    end.actor
+  end
+
+  def actors
+    @characters.map do |character|
+      character.actor
+    end
+  end
+
+end
