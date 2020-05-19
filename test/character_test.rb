54952271b2206d129e3d5ec5d7791b78e1518624
Gaby Mendez <gmendez90@gmail.com>
2020-05-19 11:09:19 -0400

    Finish iteration 1

--- test/character_test.rb
+++ test/character_test.rb
@@ -4,9 +4,24 @@ require './lib/character'
 
 class CharacterTest < Minitest::Test
 
-  def test_it_exists
-    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
-    assert_instance_of Character, kitt
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
   end
-  
+
+  def test_it_exists
+    assert_instance_of Character, @kitt
+  end
+
+  def test_it_has_a_name
+    assert_equal "KITT", @kitt.name
+  end
+
+  def test_it_has_an_actor
+    assert_equal "William Daniels", @kitt.actor
+  end
+
+  def test_it_has_a_salary
+    assert_equal 1000000, @kitt.salary
+  end
+
 end
