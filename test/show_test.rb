22d2a4b0321faa3134fa732877109180a8a5defe
Michael Chickillo <truthvendor@gmail.com>
2020-05-19 11:00:12 -0600

    Initial commit

---
+++ test/show_test.rb
@@ -0,0 +1,47 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+
+class ShowTest < Minitest::Test
+
+  def test_it_has_attributes
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_instance_of Show, knight_rider
+    assert_equal "Knight Rider", knight_rider.name
+    assert_equal "Glen Larson", knight_rider.creator
+    assert_equal [michael_knight, kitt], knight_rider.characters
+  end
+
+  def test_it_has_a_total_salary
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal 2600000, knight_rider.total_salary
+  end
+
+  def test_it_has_the_highest_salary
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal "David Hasselhoff", knight_rider.highest_paid_actor
+  end
+
+  def test_all_the_actors
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal ["David Hasselhoff", "William Daniels"], knight_rider.actors
+  end
+
+end
