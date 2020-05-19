557d41967856c4c4678e6ad7c16bf24473e951ea
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-19 09:35:45 -0600

    actors method complete with valid test

--- lib/show.rb
+++ lib/show.rb
@@ -17,10 +17,16 @@ class Show
   end
 
   def highest_paid_actor
-    max_salary =@characters.max_by do |character|
+    max_salary = @characters.max_by do |character|
       character.salary
     end
     max_salary.actor
+  end
+
+  def actors
+    @characters.map do |character|
+      character.actor
+    end
   end
 
 end
