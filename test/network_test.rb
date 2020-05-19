70028cfd3dc8cfb60a42c75cf66fb9885930c9a8
Phillip Strom <phillip.strom@gmail.com>
2020-05-19 10:00:55 -0600

    Add all the necessary @ for setup method...

--- test/network_test.rb
+++ test/network_test.rb
@@ -9,10 +9,10 @@ class NetworkTest < Minitest::Test
     @nbc = Network.new("NBC")
     @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
-    @knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
     @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
-    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
   end
 
   def test_it_exists
@@ -27,9 +27,9 @@ class NetworkTest < Minitest::Test
   end
 
   def test_add_show
-    nbc.add_show(knight_rider)
-    nbc.add_show(parks_and_rec)
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
 
-    assert_equal [knight_rider, parks_and_rec], nbc.shows
+    assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
   end
 end
