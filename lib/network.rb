fe84c010c6f27f04febe6f1674e1ad561ee36291
“Taija” <taija.warbelow@google.com>
2020-05-19 09:38:39 -0700

    add weird method for main_characters

--- lib/network.rb
+++ lib/network.rb
@@ -11,6 +11,17 @@ class Network
     shows << new_show
   end
 
+  # don't like this method, think there must be a better way
+  def main_characters
+    shows.map do |show|
+      show.characters.map do |character|
+        if character.name == character.name.upcase && character.salary > 500000
+          return character
+        end
+      end
+    end
+  end
+
   private
   attr_writer :shows
 end
