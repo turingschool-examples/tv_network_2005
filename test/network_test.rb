4c10334dd21681a38451384f3bf04eda268b5173
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-19 10:31:10 -0600

    test written for shows_by_actor method

--- test/network_test.rb
+++ test/network_test.rb
@@ -59,4 +59,26 @@ class NetworkTest < Minitest::Test
     assert_equal expected, nbc.actors_by_show
   end
 
+  def test_shows_by_actor
+    nbc = Network.new("NBC")
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    mitch = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000})
+    baywatch = Show.new("Baywatch", "Gregory Bonann", [mitch])
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    nbc.add_show(knight_rider)
+    nbc.add_show(baywatch)
+    nbc.add_show(parks_and_rec)
+    expected = {
+                  "David Hasselhoff" => [knight_rider, baywatch],
+                  "William Daniels" => [knight_rider],
+                  "Amy Poehler" => [parks_and_rec],
+                  "Nick Offerman" => [parks_and_rec]
+                }
+    assert_equal expected, nbc.shows_by_actor 
+  end
+
 end
