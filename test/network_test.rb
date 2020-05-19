ea9e08c7108bcd2b0c2408f9c8e78464327aef63
Sienna Kopf <princess.kopf@gmail.com>
2020-05-19 10:01:34 -0600

    Add tests for show functioanlity add show, main chars, and group actors

--- test/network_test.rb
+++ test/network_test.rb
@@ -7,6 +7,14 @@ require './lib/network'
 class NetworkTest < Minitest::Test
   def setup
     @nbc = Network.new("NBC")
+
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
   end
 
   def test_it_exists
@@ -19,5 +27,27 @@ class NetworkTest < Minitest::Test
 
   def test_it_is_initialized_with_no_shows
     assert_equal [], @nbc.shows
+  end
+
+  def test_it_can_add_show
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+    assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
+  end
+
+  def test_it_has_main_characters
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+    assert_equal @kitt, @nbc.main_characters
+  end
+
+  def test_it_groups_actors_by_show
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+    grouped_actors = {
+      @knight_rider => ["David Hasselhoff", "William Daniels"],
+      @parks_and_rec => ["Amy Poehler", "Nick Offerman"]
+    }
+    assert_equal grouped_actors, @nbc.actors_by_show
   end
 end
