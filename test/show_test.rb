dc57060c2ec044fb457b6af60726fb1a498b4299
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-19 11:17:24 -0400

    Add tests for attributes

--- test/show_test.rb
+++ test/show_test.rb
@@ -12,5 +12,27 @@ class ShowTest < MiniTest::Test
     assert_instance_of Show, knight_rider
   end
 
-  
+  def test_it_has_a_name
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal "Knight Rider", knight_rider.name
+  end
+
+  def test_it_has_a_creator
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal "Glen Larson", knight_rider.creator
+  end
+
+  def test_it_has_characters
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal [michael_knight, kitt], knight_rider.characters
+  end
 end
