3f7a00e5577c4af0fa6ad67546529d4484ce2315
James Belta <james.belta@gmail.com>
2020-05-19 09:56:23 -0600

    Build setup with instance assertion

---
+++ test/network_test.rb
@@ -0,0 +1,38 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+require './lib/network'
+
+class NetworkTest < Minitest::Test
+  def setup
+    @nbc = Network.new("NBC")
+    @kitt = Character.new({
+                          name: "KITT",
+                          actor: "William Daniels",
+                          salary: 1_000_000
+                          })
+    @michael_knight = Character.new({
+                                      name: "Michael Knight",
+                                      actor: "David Hasselhoff",
+                                      salary: 1_600_000
+                                      })
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+
+    @leslie_knope = Character.new({
+                                    name: "Leslie Knope",
+                                    actor: "Amy Poehler",
+                                    salary: 2_000_000
+                                    })
+    @ron_swanson = Character.new({
+                                    name: "Ron Swanson",
+                                    actor: "Nick Offerman",
+                                    salary: 1_400_000
+                                    })
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+  end
+
+  def test_it_exist
+    assert_instance_of Network, @nbc
+  end
+end
