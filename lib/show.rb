8ec1526614f846301a3177b1d1223a4bc0494819
“Taija” <taija.warbelow@google.com>
2020-05-19 08:32:27 -0700

    attr_reader and initialize Show class

---
+++ lib/show.rb
@@ -0,0 +1,11 @@
+class Show
+  attr_reader :name,
+              :creator,
+              :characters
+
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+end
