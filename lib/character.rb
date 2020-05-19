12b1fc00eab9725eee5b42f89e242e3d8184d19b
Eric Hale <eric_hale@comcast.net>
2020-05-19 09:13:35 -0600

    Add attributes methods and test

--- lib/character.rb
+++ lib/character.rb
@@ -1,5 +1,8 @@
 class Character
+  attr_reader :name, :actor, :salary
   def initialize(character_info)
-    @character_info = character_info
+    @name = character_info[:name]
+    @actor = character_info[:actor]
+    @salary = character_info[:salary]
   end
 end
--- test/character_test.rb
+++ test/character_test.rb
@@ -10,4 +10,10 @@ class CharacterTest < Minitest::Test
   def test_it_exists
     assert_instance_of Character, @kitt
   end
+
+  def test_it_has_attributes
+    assert_equal "KITT", @kitt.name
+    assert_equal "William Daniels", @kitt.actor
+    assert_equal 1_000_000, @kitt.salary
+  end
 end
