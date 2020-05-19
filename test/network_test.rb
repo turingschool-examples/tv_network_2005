945a92674a64368f03f28078f8e31340fc7e25e3
Phillip Strom <phillip.strom@gmail.com>
2020-05-19 09:59:25 -0600

    Build add_show test and fix setup method

--- test/network_test.rb
+++ test/network_test.rb
@@ -6,13 +6,13 @@ require 'minitest/pride'
 
 class NetworkTest < Minitest::Test
   def setup
-    nbc = Network.new("NBC")
-    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
-    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
-    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
-    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
-    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
-    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    @nbc = Network.new("NBC")
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
   end
 
   def test_it_exists
@@ -24,5 +24,12 @@ class NetworkTest < Minitest::Test
     nbc = Network.new("NBC")
     assert_equal "NBC", nbc.name
     assert_equal [], nbc.shows
+  end
+
+  def test_add_show
+    nbc.add_show(knight_rider)
+    nbc.add_show(parks_and_rec)
+
+    assert_equal [knight_rider, parks_and_rec], nbc.shows
   end
 end
