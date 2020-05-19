679c6fae3363ef8f9be76484fa50ea2f25232eb8
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-19 09:35:08 -0600

    show total salary

---
+++ lib/show.rb
@@ -0,0 +1,14 @@
+class Show
+  attr_reader :name, :creator, :characters
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    @characters.sum do |character|
+      character.salary
+    end
+  end
+end
