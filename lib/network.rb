e16e36f2b9bb07bfe0f96d5117523ad24222ef15
Michael Evans <michaeljevans113@gmail.com>
2020-05-19 09:58:31 -0600

    Add main_characters method and associated testing

--- lib/network.rb
+++ lib/network.rb
@@ -9,4 +9,16 @@ class Network
   def add_show(show)
     @shows << show
   end
+
+  def main_characters
+    all_characters = []
+    main = []
+    @shows.each do |show|
+      show.characters.each {|character| all_characters << character}
+    end
+    all_characters.each do |character|
+      main << character if character.name == character.name.upcase
+    end
+    main
+  end
 end
