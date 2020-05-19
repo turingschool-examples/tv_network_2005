a437ff0641effd3e58e7e5b162ed497170f48b3c
Norma Lopez <norma_l304@yahoo.com>
2020-05-19 09:36:34 -0600

    Add test for starting off with no shows

--- test/network_test.rb
+++ test/network_test.rb
@@ -8,6 +8,12 @@ class NetworkTest < Minitest::Test
   def test_it_exists
     nbc = Network.new("NBC")
 
-    assert_instance_of Network, nbc 
+    assert_instance_of Network, nbc
+  end
+
+  def test_it_starts_with_no_shows
+    nbc = Network.new("NBC")
+
+    assert_equal [], nbc.shows
   end
 end
