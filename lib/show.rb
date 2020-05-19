44a999e69eeedac1c9e0758095e9d7cc70146cbb
“Taija” <taija.warbelow@google.com>
2020-05-19 08:42:56 -0700

    Add method for highest_paid_actor

--- lib/show.rb
+++ lib/show.rb
@@ -14,4 +14,10 @@ class Show
       character.info[:salary]
     end
   end
+
+  def highest_paid_actor
+    characters.max_by do |character|
+      character.info[:salary]
+    end.actor
+  end
 end
