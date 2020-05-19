f948c8a22dbc59ea801e53d38272ee1e5c117f23
Sienna Kopf <princess.kopf@gmail.com>
2020-05-19 10:00:57 -0600

    Add show functionality for add show, main chars and actors by show

--- lib/network.rb
+++ lib/network.rb
@@ -7,4 +7,25 @@ class Network
     @shows = []
   end
 
+  def add_show(show)
+    shows << show
+  end
+
+  def main_characters
+    shows.each do |show|
+      show.characters.each do |character|
+        if character.salary > 500_000 && character.name == character.name.upcase
+          return character
+        end
+      end
+    end
+  end
+
+  def actors_by_show ## FIX ME
+    {
+      @shows[0] => @shows[0].actors,
+      @shows[1] => @shows[1].actors
+    }
+  end
+
 end
