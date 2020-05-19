f746f4f418a83e3a9f767d6ccc7d67bbdf8c2462
Sienna Kopf <princess.kopf@gmail.com>
2020-05-19 11:01:23 -0600

    Add functionality for grouping by show and attempt for grouping shows by actor and prolific actor

--- lib/network.rb
+++ lib/network.rb
@@ -21,11 +21,39 @@ class Network
     end
   end
 
-  def actors_by_show ## FIX ME
+  def actors_by_show ## INFLEXIBLE
     {
       @shows[0] => @shows[0].actors,
       @shows[1] => @shows[1].actors
     }
+  end
+
+  def shows_by_actor
+    ## throwing a no type error... no idea why beacause it works in a pry session 
+   starting_point = {
+     "David Hasselhoff" => [],
+     "William Daniels" => [],
+     "Amy Poehler" => [],
+     "Nick Offerman" => []
+   }
+
+   @shows.reduce(starting_point) do |acc, show|
+     show.characters.each do |character|
+       if character.actor == "David Hasselhoff"
+         acc["David Hasselhoff"] << show
+       elsif character.actor == "William Daniels"
+          acc["William Daniels"] << show
+       elsif character.actor == "Amy Poehler"
+          acc["Amy Poehler"] << show
+       elsif character.actor == "Nick Offerman"
+          acc["Nick Offerman"] << show
+       end
+       acc
+     end
+   end
+  end
+
+  def prolific_actors
   end
 
 end
--- test/network_test.rb
+++ test/network_test.rb
@@ -12,8 +12,12 @@ class NetworkTest < Minitest::Test
     @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
 
+    @mitch = Character.new({name: "Mitch Buchannon", actor: "David Hasselhoff", salary: 1_200_000})
+    @baywatch = Show.new("Baywatch", "Gregory Bonann", [@mitch])
+
     @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
     @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
   end
 
@@ -49,5 +53,26 @@ class NetworkTest < Minitest::Test
       @parks_and_rec => ["Amy Poehler", "Nick Offerman"]
     }
     assert_equal grouped_actors, @nbc.actors_by_show
+  end
+
+  def test_it_groups_shows_by_actor
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@baywatch)
+    @nbc.add_show(@parks_and_rec)
+    grouped_shows = {
+      "David Hasselhoff" => [@knight_rider, @baywatch],
+      "William Daniels" => [@knight_rider],
+      "Amy Poehler" => [@parks_and_rec],
+      "Nick Offerman" => [@parks_and_rec]
+    }
+    assert_equal grouped_shows, @nbc.shows_by_actor
+  end
+
+  def test_it_has_prolific_actors
+    skip
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@baywatch)
+    @nbc.add_show(@parks_and_rec)
+    assert_equal ["David Hasselhoff"], @nbc.prolific_actors
   end
 end
