002c773122010dde69213ed59876bf9886be7dde
AJ Jamie Tran <anh.jamietran@gmail.com>
2020-05-19 10:21:30 -0600

    Add test for actors by show

--- test/network_test.rb
+++ test/network_test.rb
@@ -36,10 +36,18 @@ class NetworkTest < MiniTest::Test
 
     assert_equal [@kitt], @nbc.main_characters
   end
-end
 
-# nbc.actors_by_show
-# {
-      #<Show:0x00007fe5f8398970...> ["David Hasselhoff", "William Daniels"],
-      #<Show:0x00007fe5f88b0a20...> ["Amy Poehler", "Nick Offerman"]
-#    }
+  def test_it_know_actors_by_show
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    expected = {
+                @knight_rider => ["David Hasselhoff", "William Daniels"],
+                @parks_and_rec => ["Amy Poehler", "Nick Offerman"]
+                }
+
+    assert_equal expected, @nbc.actors_by_show
+  end
+
+
+end
