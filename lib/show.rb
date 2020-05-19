2e445003fcc16f469dd7f472534cc2ea60ff4f14
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-19 11:38:18 -0400

    Add actors test and method

--- lib/show.rb
+++ lib/show.rb
@@ -13,4 +13,12 @@ class Show
   def highest_paid_actor
     (characters.max_by { |character| character.salary }).actor
   end
+
+  def actors
+    actors = []
+    characters.select do |char|
+      actors << char.actor
+    end
+    actors
+  end
 end
