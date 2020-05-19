30ab76e6b29108f57d9129c4813e71865d02730d
Chandler Hulstrom <chulstro11@gmail.com>
2020-05-19 09:24:16 -0600

    Inital Show class test complete

---
+++ lib/show.rb
@@ -0,0 +1,15 @@
+require 'pry'
+
+class Show
+  attr_reader :name,
+              :creator,
+              :characters
+
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+
+end
