4e110a6490566ddae598597feafcf4117102d946
Kiera Allen <kieraaallen@gmail.com>
2020-05-19 10:29:43 -0600

    Add add_show method and test

--- test/network_test.rb
+++ test/network_test.rb
@@ -21,33 +21,24 @@ class NetworkTest < Minitest::Test
   def test_it_starts_with_no_shows
     assert_equal [], @nbc.shows
   end
+
+  def test_it_can_add_shows
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+
+    @nbc.add_show(knight_rider)
+    @nbc.add_show(parks_and_rec)
+
+    assert_equal [knight_rider, parks_and_rec], @nbc.shows
+  end
 end
 
-# pry(main)> michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
-# # => #<Character:0x00007fe5f88721f8...>
-#
-# pry(main)> kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
-# # => #<Character:0x00007fe5f8448f78...>
-#
-# pry(main)> knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
-# # => #<Show:0x00007fe5f8398970...>
-#
-# pry(main)> leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
-# # => #<Character:0x00007fe5f832bb18...>
-#
-# pry(main)> ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
-# # => #<Character:0x00007fe5f8172a60...>
-#
-# pry(main)> parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
-# # => #<Show:0x00007fe5f88b0a20...>
-#
-# pry(main)> nbc.add_show(knight_rider)
-#
-# pry(main)> nbc.add_show(parks_and_rec)
-#
-# pry(main)> nbc.shows
-# # => [#<Show:0x00007fe5f8398970...>, #<Show:0x00007fe5f88b0a20...>]
-#
+
 # pry(main)> nbc.main_characters
 # # => [#<Character:0x00007f98a4ba8dc8...>]
 #
