a78a303a91ab6af969c3f8ad4a675379ae24b1d8
Phillip Strom <phillip.strom@gmail.com>
2020-05-19 10:01:42 -0600

    Create add_show method

--- lib/network.rb
+++ lib/network.rb
@@ -3,9 +3,14 @@ require './lib/show'
 
 class Network
   attr_reader :name, :shows
-  
+
   def initialize(name)
     @name = name
     @shows = []
   end
+
+  def add_show(show)
+    @shows << show
+  end
+
 end
