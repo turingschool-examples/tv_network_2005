b11b90445f427b6c0e983b39b629e441bffb76b3
James Belta <james.belta@gmail.com>
2020-05-19 09:12:48 -0600

    Build attributes test

--- test/character_test.rb
+++ test/character_test.rb
@@ -12,4 +12,16 @@ class CharacterTest < Minitest::Test
                           })
     assert_instance_of Character, kitt
   end
+
+  def test_it_has_attributes
+    kitt = Character.new({
+                          name: "KITT",
+                          actor: "William Daniels",
+                          salary: 1_000_000
+                          })
+
+    assert_equal "KITT", kitt.name
+    assert_equal "William Daniels", kitt.actor
+    assert_equal 1_000_000, kitt.salary
+  end
 end
