85cf8243b0be9e43f269a1aea7be2583de32817c
James Belta <james.belta@gmail.com>
2020-05-19 09:23:37 -0600

    Build Show class, Tested instance

---
+++ test/show_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character.rb'
+require './lib/show'
+
+class ShowTest < Minitest::Test
+  def setup
+    @kitt = Character.new({
+                          name: "KITT",
+                          actor: "William Daniels",
+                          salary: 1_000_000
+                          })
+    @michael_knight = Character.new({
+                                      name: "Michael Knight",
+                                      actor: "David Hasselhoff",
+                                      salary: 1_600_000
+                                      })
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+  end
+
+  def test_it_exist
+    assert_instance_of Show, @knight_rider
+  end
+end
