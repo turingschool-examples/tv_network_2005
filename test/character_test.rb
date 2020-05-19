d2790114df35f3457f84eef566e433b78cf9888d
Alex Desjardins <ardesjardins91@gmail.com>
2020-05-19 09:22:20 -0600

    Character test complete

--- test/character_test.rb
+++ test/character_test.rb
@@ -10,13 +10,19 @@ class CharacterTest < Minitest::Test
     assert_instance_of Character, kitt
   end
 
-  def test_player_has_name
+  def test_character_name
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
     assert_equal "KITT", kitt.name
   end
 
+  def test_character_actor
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    assert_equal "William Daniels", kitt.actor
+  end
 
-
-
+  def test_character_salary
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    assert_equal 1000000, kitt.salary
+  end
 
 end
