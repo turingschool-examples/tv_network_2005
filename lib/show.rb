161c6e1d18ae0eea34845b887ef858c1b9e7dcf7
Ryan Laleh <laleh21@yahoo.com>
2020-05-19 10:21:19 -0600

    All test pass but one for show files

---
+++ lib/show.rb
@@ -0,0 +1,25 @@
+class Show
+  attr_reader :name, :creator, :characters
+
+  def initialize(name, creator, characters)
+    @name = name
+    @creator = creator
+    @characters = characters
+  end
+
+  def total_salary
+    @characters.sum do |character|
+      character.salary
+    end
+  end
+
+  def highest_paid_actor
+    @characters.max_by do |char|
+      char.salary
+    end
+  end
+
+  def actor
+    @charactors[:actor]
+  end
+end
