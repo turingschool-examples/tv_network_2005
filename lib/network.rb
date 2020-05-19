870912d3a52188cf25e6d32c3feb60f0454d2861
Phillip Strom <phillip.strom@gmail.com>
2020-05-19 10:18:07 -0600

    Create main_characters method

--- lib/network.rb
+++ lib/network.rb
@@ -13,4 +13,16 @@ class Network
     @shows << show
   end
 
+  def main_characters
+    main_characters = []
+    @shows.each do |show|
+      show.characters.each do |character|
+        if character.salary > 500_000 && character.name == character.name.upcase
+          main_characters << character
+        end
+      end
+    end
+    main_characters
+  end
+
 end
