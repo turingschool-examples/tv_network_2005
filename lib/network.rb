f90a5dd97d939bac216b2f44958de2074b260e5b
Chandler Hulstrom <chulstro11@gmail.com>
2020-05-19 10:12:35 -0600

    main_characters accessable

--- lib/network.rb
+++ lib/network.rb
@@ -8,4 +8,19 @@ class Network
     @name = name
     @shows =[]
   end
+
+  def add_show(show)
+    @shows << show
+  end
+
+  def main_characters
+    main_characters = []
+    @shows.each do |show|
+      # binding.pry
+      main_characters << show.characters.find_all do |character|
+        character.salary > 500_00 && character.name.eql?(character.name.upcase)
+      end
+    end
+    main_characters.flatten
+  end
 end
--- test/network_test.rb
+++ test/network_test.rb
@@ -8,10 +8,30 @@ class NetworkTest < Minitest::Test
 
   def setup
     @nbc = Network.new("NBC")
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+    @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
+    @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
   end
 
   def test_initial_values
     assert_equal "NBC", @nbc.name
     assert_equal [], @nbc.shows
+  end
+
+  def test_it_can_add_shows
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    assert_equal [@knight_rider, @parks_and_rec], @nbc.shows
+  end
+
+  def test_access_main_character
+    @nbc.add_show(@knight_rider)
+    @nbc.add_show(@parks_and_rec)
+
+    assert_equal [@kitt], @nbc.main_characters
   end
 end
