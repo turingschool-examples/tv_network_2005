97b6390e704ddd448d8315486706c2bae861d001
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-19 11:07:30 -0400

    Add attributes

--- lib/character.rb
+++ lib/character.rb
@@ -1,4 +1,8 @@
 class Character
-  def initialize
+  attr_reader :name, :actor, :salary
+  def initialize(info)
+    @name = info[:name]
+    @actor = info[:actor]
+    @salary = info[:salary]
   end
 end
