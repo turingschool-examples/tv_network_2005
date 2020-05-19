5b607433580f05d14acba5a7585eef5563df08b5
Kwibe Merci <jeremiahkwibe@hotmail.com>
2020-05-19 09:14:47 -0600

    test for character

---
+++ test/character_test.rb
@@ -0,0 +1,29 @@
+require "minitest/autorun"
+require "./lib/character"
+
+class CharacterTest < Minitest::Test
+  def test_it_exists
+    character = Character.new("KITT", "William Daniels", 1_000_000)
+
+    assert_instance_of Character, character
+  end
+
+  def test_character_has_name
+    kitt = Character.new("KITT", "William Daniels", 1_000_000)
+
+    assert_equal "KITT", kitt.name
+  end
+
+  def test_character_has_actor
+    kitt = Character.new("KITT", "William Daniels", 1_000_000)
+
+    assert_equal "William Daniels", kitt.actor
+  end
+
+  def test_character_has_name
+    kitt = Character.new("KITT", "William Daniels", 1_000_000)
+
+    assert_equal 1_000_000, kitt.salary
+  end
+
+end
