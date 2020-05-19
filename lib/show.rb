535fa5cc8ec590c49a8a5468b9ea25b5cfd77b61
Dani Coleman <danicoleman00@gmail.com>
2020-05-19 10:41:48 -0600

    Add show class

---
+++ lib/show.rb
@@ -0,0 +1,17 @@
+class Show
+  attr_reader :name,
+              :creator,
+              :characters
+
+  def initialize(show_name, show_creator, show_characters)
+    @name = show_name
+    @creator = show_creator
+    @characters = show_characters
+
+  end
+
+  def total_salary
+    
+  end
+
+end
