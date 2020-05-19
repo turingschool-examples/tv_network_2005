00e5abb801d15a51d7e1191705990ca375f5ef82
Amy Moller <amy.moller@yahoo.com>
2020-05-19 10:14:42 -0600

    Add more setup

--- test/show_test.rb
+++ test/show_test.rb
@@ -1,12 +1,15 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/character'
+require './lib/show'
 
 class ShowTest < Minitest::Test
 
   def setup
+    kitt = Character_1.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character_2.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
 
-  end 
+  end
 
 
 
