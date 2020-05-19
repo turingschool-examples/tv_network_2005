d59596dcd5aa8f2a602f20e92ebaeaa28b234516
Kiera Allen <kieraaallen@gmail.com>
2020-05-19 09:29:42 -0600

    Create Show test file and test existence and attributes

---
+++ test/show_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+require './lib/show'
+
+class CharacterTest < Minitest::Test
+
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
+  end
+
+  def test_it_exists
+    assert_instance_of Show, @knight_rider
+  end
+
+  def test_it_can_read_attributes
+    assert_equal "Knight Rider", @knight_rider.name
+    assert_equal "Glen Larson", @knight_rider.creator
+    assert_equal [@michael_knight, @kitt], @knight_rider.characters
+  end
+end
+
+
+# pry(main)> knight_rider.total_salary
+# # => 2600000
+#
+# pry(main)> knight_rider.highest_paid_actor
+# # => "David Hasselhoff"
+#
+# pry(main)> knight_rider.actors
+# # => ["David Hasselhoff", "William Daniels"]
