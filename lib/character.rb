fce7b0b3641decbd3c19deafd6c28f8f0993ea84
Phillip Strom <phillip.strom@gmail.com>
2020-05-20 10:51:18 -0600

    update attributes

--- lib/character.rb
+++ lib/character.rb
@@ -1,7 +1,8 @@
 class Character
-  attr_reader :name, :actor, :salary
+  attr_reader :name, :actor, :salary, :character_info
 
   def initialize(character_info)
+    @character_info = character_info
     @name = character_info[:name]
     @actor = character_info[:actor]
     @salary = character_info[:salary]
