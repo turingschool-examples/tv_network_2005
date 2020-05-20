c7f58b8c6e990774a6decd90923a11d570695dcb
Phillip Strom <phillip.strom@gmail.com>
2020-05-20 10:51:45 -0600

    refactor highest paid actor

--- lib/show.rb
+++ lib/show.rb
@@ -10,18 +10,24 @@ attr_reader :name, :creator, :characters
   end
 
   def total_salary
-    salary_total = 0
-    @characters.each do |character|
-      salary_total += character.salary
+    # salary_total = 0
+    # @characters.each do |character|
+    #   salary_total += character.salary
+    # end
+    # salary_total
+    characters.sum do |character| # refactored solution
+      character.character_info[:salary]
     end
-    salary_total
   end
 
   def highest_paid_actor
-    highest_paid = @characters.max do |a, b|
-      a.salary<=>b.salary
-    end
-    highest_paid.actor
+    # highest_paid = @characters.max do |a, b|
+    #   a.salary<=>b.salary
+    # end
+    # highest_paid.actor
+    characters.max_by do |character| # refactored solution
+      character.salary
+    end.actor
   end
 
   def actors
