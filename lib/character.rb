36ec4695b5fd9ff0960c94a6a302eeeacc5fe544
memcmahon <mcmahon.meganelizabeth@gmail.com>
2020-05-20 11:23:52 -0600

    Megan's Walkthrough

---
+++ lib/character.rb
@@ -0,0 +1,9 @@
+class Character
+  attr_reader :name, :actor, :salary
+
+  def initialize(character_params)
+    @name = character_params[:name]
+    @actor = character_params[:actor]
+    @salary = character_params[:salary]
+  end
+end
---
+++ test/character_test.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+
+class CharacterTest < Minitest::Test
+  def setup
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+  end
+
+  def test_it_exists
+    assert_instance_of Character, @kitt
+  end
+
+  def test_it_has_a_name
+    assert_equal "KITT", @kitt.name
+  end
+
+  def test_it_has_an_actor
+    assert_equal "William Daniels", @kitt.actor
+  end
+
+  def test_it_has_a_salary
+    assert_equal 1000000, @kitt.salary
+  end
+end
